18dd4cdcf74e6e3ccfe41fdec2ca9e260d4fadf8
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-14 01:19:42 +0530

    feat: integerated the login signup page with conditions for for mobile & desktop

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { assets } from "./../../assets/assets";
 import { NavLink, useLocation } from "react-router-dom";
 import ThemeToggle from "./../theme/ThemeToggle";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const location = useLocation();
   const isCourseListPage = location.pathname.includes("/course-list");
+  const { openSignIn } = useClerk();
+  const user = useUser();
+  console.log(user);
 
   return (
     <div
@@ -24,31 +28,48 @@ const Navbar = () => {
 
       {/* Desktop Navigation */}
       <div className="hidden md:flex items-center gap-6 text-gray-700 dark:text-white">
-        <button className="hover:underline">Become Educator</button>
-        <NavLink to="/my-enrollments" className="hover:underline">
-          My Enrollments
-        </NavLink>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition">
-          Create Account
-        </button>
+        {user?.isSignedIn && (
+          <>
+            <button className="hover:underline">Become Educator</button>
+            <NavLink to="/my-enrollments" className="hover:underline">
+              My Enrollments
+            </NavLink>
+          </>
+        )}
+        {user?.isSignedIn ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition"
+          >
+            Create Account
+          </button>
+        )}
         <ThemeToggle />
       </div>
 
       {/* Mobile Navigation */}
       <div className="md:hidden flex items-center gap-3 text-gray-700 dark:text-white">
+        {user?.isSignedIn &&
+        <>
         <button className="hover:underline text-sm sm:text-base">
           Become Educator
         </button>
-        <NavLink to="/my-enrollments" className="hover:underline text-sm sm:text-base">
+        <NavLink
+          to="/my-enrollments"
+          className="hover:underline text-sm sm:text-base"
+        >
           My Enrollments
         </NavLink>
-        <button>
+        </>}
+        {user?.isSignedIn? <UserButton /> : <button onClick={() => openSignIn()}>
           <img
             src={assets.user_icon}
             alt="User"
             className="w-6 h-6 object-contain"
           />
-        </button>
+        </button>}
         <ThemeToggle />
       </div>
     </div>
