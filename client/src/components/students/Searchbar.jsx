7f65aac784198cd3bfbcb0b4244d20b1097e4e95
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-14 02:08:32 +0530

    feat(searchbar): add searchbar logic to handle searches

--- client/src/components/students/Searchbar.jsx
+++ client/src/components/students/Searchbar.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const Searchbar = () => {
+const Searchbar = ({ data }) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : "");
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    navigate(`/course-list/${input}`);
+  };
+
   return (
-    <form className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white dark:bg-gray-800 border border-gray-500/20 dark:border-gray-600 rounded transition-colors duration-300">
+    <form
+      onSubmit={onSearchHandler}
+      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white dark:bg-gray-800 border border-gray-500/20 dark:border-gray-600 rounded transition-colors duration-300"
+    >
       <img
         src={assets.search_icon}
         alt="searchIcon"
         className="md:w-auto w-10 px-3 invert-0 dark:invert"
       />
       <input
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
         type="text"
         placeholder="Search for courses"
         className="w-full h-full outline-none text-gray-500/80 dark:text-white dark:placeholder-gray-400 bg-transparent"
