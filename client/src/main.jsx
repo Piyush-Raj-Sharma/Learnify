d432fb9e8bca2a607b3770baf16730bcaabbff26
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-14 00:52:04 +0530

    Clerk setup for Authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,24 @@ import "./index.css";
 import App from "./App.jsx";
 import { AppContextProvider } from "./context/AppContext.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { ClerkProvider } from "@clerk/clerk-react";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <AppContextProvider>
-      <App />
-    </AppContextProvider>
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <Provider store={store}>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </Provider>
+    </ClerkProvider>
   </BrowserRouter>
 );
