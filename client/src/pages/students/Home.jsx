2b2935a3418de961422b7d473886b9d464704db3
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-14 02:24:34 +0530

    feat(companies): add trusted-by-companies section

--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -1,9 +1,11 @@
+import Companies from "../../components/students/Companies";
 import Hero from "../../components/students/Hero";
 
 
 const Home = () => {
-  return <div className="'flex flex-col items-center space-y-7 text-center">
+  return <div className="'flex flex-col items-center space-y-7 text-center dark:bg-gray-950">
     <Hero/>
+    <Companies/>
   </div>;
 };
 
