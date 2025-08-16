1aa3a25e40e9974048adb96a005793b7a0a82a01
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-08-16 12:38:22 +0530

    Add read more btn in Testimonial section cards

--- client/src/components/students/Testimonial.jsx
+++ client/src/components/students/Testimonial.jsx
@@ -42,6 +42,7 @@ const Testimonial = () => {
               </div>
               <p className="text-gray-500 mt-5 dark:text-white">{testimonial.feedback}</p>
             </div>
+            <a href="#" className="text-blue-500 underline px-5">Read More</a>
           </div>
         ))} 
       </div>
